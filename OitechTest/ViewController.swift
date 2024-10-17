7b098936f8b138f370e26790eba4c8cccad3ace1
Salimzhann <manassalimzhan@icloud.com>
2024-10-17 14:27:20 +0500

    Initial Commit

---
+++ OitechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OitechTest
+//
+//  Created by Manas Salimzhan on 17.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
